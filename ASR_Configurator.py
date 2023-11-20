92392e80820173a3049c1b8941e79cf00c1d1739
Michael Haag <5632822+MHaggis@users.noreply.github.com>
2023-11-20 12:00:30 -0700

    Add Webshell and Capitals

--- ASR_Configurator.py
+++ ASR_Configurator.py
@@ -3,22 +3,23 @@ import streamlit as st
 st.set_page_config(page_title="ASR Configurator", layout="wide")
 
 asr_rules = {
-    "Block abuse of exploited vulnerable signed drivers": "56a863a9-875e-4185-98a7-b882c64b5ce5",
-    "Block Adobe Reader from creating child processes": "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c",
+    "Block abuse of exploited vulnerable signed drivers": "56A863A9-875E-4185-98A7-B882C64B5CE5",
+    "Block Adobe Reader from creating child processes": "7674BA52-37EB-4A4F-A9A1-F0F9A1619A2C",
     "Block all Office applications from creating child processes": "D4F940AB-401B-4EFC-AADC-AD5F3C50688A",
-    "Block credential stealing from the Windows local security authority subsystem (lsass.exe)": "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2",
+    "Block credential stealing from the Windows local security authority subsystem (lsass.exe)": "9E6C4E1F-7D60-472F-BA1A-A39EF669E4B2",
     "Block executable content from email client and webmail": "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550",
-    "Block executable files from running unless they meet a prevalence, age, or trusted list criterion": "01443614-cd74-433a-b99e-2ecdc07bfc25",
+    "Block executable files from running unless they meet a prevalence, age, or trusted list criterion": "01443614-CD74-433A-B99E-2ECDC07BFC25",
     "Block execution of potentially obfuscated scripts": "5BEB7EFE-FD9A-4556-801D-275E5FFC04CC",
     "Block JavaScript or VBScript from launching downloaded executable content": "D3E037E1-3EB8-44C8-A917-57927947596D",
     "Block Office applications from creating executable content": "3B576869-A4EC-4529-8536-B80A7769E899",
     "Block Office applications from injecting code into other processes": "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84",
-    "Block Office communication application from creating child processes": "26190899-1602-49e8-8b27-eb1d0a1ce869",
-    "Block persistence through WMI event subscription": "e6db77e5-3df2-4cf1-b95a-636979351e5b",
-    "Block process creations originating from PSExec and WMI commands": "d1e49aac-8f56-4280-b9ba-993a6d77406c",
-    "Block untrusted and unsigned processes that run from USB": "b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4",
+    "Block Office communication application from creating child processes": "26190899-1602-49E8-8B27-EB1D0A1CE869",
+    "Block persistence through WMI event subscription": "E6DB77E5-3DF2-4CF1-B95A-636979351E5B",
+    "Block process creations originating from PSExec and WMI commands": "D1E49AAC-8F56-4280-B9BA-993A6D77406C",
+    "Block untrusted and unsigned processes that run from USB": "B2B3F03D-6A65-4F7B-A9C7-1C7EF74A9BA4",
     "Block Win32 API calls from Office macros": "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B",
-    "Use advanced protection against ransomware": "c1db55ab-c21a-4637-bb3f-a12568109d35"
+    "Use advanced protection against ransomware": "C1DB55AB-C21A-4637-BB3F-A12568109D35",
+    "Block Webshell creation for Servers": "A8F5898E-1DC8-49A9-9878-85004B8A61E6"
 }
 
 st.title("Attack Surface Configurator")
